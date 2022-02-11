ef427bf57f761b6b8403612239ae90a4f3927ac1
Joe Chin <joe.chin.jia.hao@gmail.com>
2022-02-11 09:45:12 +0800

    updates

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -5,14 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-
+import { RecipeListComponentComponent } from './recipe-list-component/recipe-list-component.component';
+import { RecipeDetailComponentComponent } from './recipe-detail-component/recipe-detail-component.component';
+import { RecipeAddComponentComponent } from './recipe-add-component/recipe-add-component.component';
+import { RouterModule } from '@angular/router';
+import {HttpClientModule} from '@angular/common/http'
+const appRoutes=[
+          {path:'',component:RecipeListComponentComponent},
+          {path:'recipe/:recipeId',component:RecipeDetailComponentComponent},
+          {path:'add',component:RecipeAddComponentComponent},
+          {path:'**',redirectTo:""}]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeListComponentComponent,
+    RecipeDetailComponentComponent,
+    RecipeAddComponentComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the app is stable
